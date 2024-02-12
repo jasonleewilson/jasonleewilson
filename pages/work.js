167a34275d657c205eb580f340701247e6468615
Jason Wilson <wilson@Jasons-Air.home>
2024-02-12 15:57:58 -0600

    update

--- pages/work.js
+++ pages/work.js
@@ -24,14 +24,14 @@ export default function Work() {
             <div>
               <h1>Work</h1>
               <ul>
-                <li><Link href="http://lunarnewyear.com" target="_blank">LunarNewYear.com</Link></li>
-                <li><Link href="https://pagodaasiangrill.com/" target="_blank">PagodaAsianGrill.com</Link></li>
-                <li><Link href="https://www.pfchangs.com/" target="_blank">PFChangs.com</Link></li>
-                <li><Link href="https://eptexas.com/" target="_blank">EPTexas.com</Link></li>
-                <li><Link href="https://bitcoin-current-price.com/" target="_blank">Bitcoin-Current-Price.com</Link></li>
-                <li><Link href="https://www.current-price.com/" target="_blank">Current-Price.com</Link></li>
-                <li><Link href="https://luckycatfund.com/" target="_blank">LuckyCatFund.com</Link></li>
-                <li><Link href="https://jasonleewilson.github.io/" target="_blank">jasonleewilson.github.io</Link></li>
+                <li><a href="http://lunarnewyear.com" target="_blank">LunarNewYear.com</a></li>
+                <li><a href="https://pagodaasiangrill.com/" target="_blank">PagodaAsianGrill.com</a></li>
+                <li><a href="https://www.pfchangs.com/" target="_blank">PFChangs.com</a></li>
+                <li><a href="https://eptexas.com/" target="_blank">EPTexas.com</a></li>
+                <li><a href="https://bitcoin-current-price.com/" target="_blank">Bitcoin-Current-Price.com</a></li>
+                <li><a href="https://www.current-price.com/" target="_blank">Current-Price.com</a></li>
+                <li><a href="https://luckycatfund.com/" target="_blank">LuckyCatFund.com</a></li>
+                <li><a href="https://jasonleewilson.github.io/" target="_blank">jasonleewilson.github.io</a></li>
 
               </ul>
             </div>
