8e6c0a87fe3b7f310c570a2b7a7c1c98561d0db1
Jason Wilson <wilson@Jasons-Air.home>
2024-02-12 14:18:27 -0600

    updated form

--- pages/contact.js
+++ pages/contact.js
@@ -30,7 +30,7 @@ export default function Contact() {
                 <a href="mailto:info@jasonleewilson.com">info@jasonleewilson.com</a>
               </p>
               <div>
-              <form name="contact" method="post" data-netlify="true">
+              <form name="contact" method="post" netlify>
                 <input className="d-block form-control" type="text" id="name" name="name" aria-label="name" placeholder="Name" required />
                 <input className="d-block form-control" type="email" id="email" name="email" aria-label="email" placeholder="Email" required />
                 <textarea className="d-block form-control" id="message" name="message" aria-label="message" placeholder="Message" required />
