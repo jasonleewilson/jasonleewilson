d3e85aaa7e3f12cc41a95e2656b0e3caaee61741
Jason Wilson <wilson@Jasons-Air.home>
2024-02-12 16:43:18 -0600

    update

--- pages/work.js
+++ pages/work.js
@@ -24,14 +24,14 @@ export default function Work() {
             <div>
               <h1>Work</h1>
               <ul>
-                <li><a href="http://lunarnewyear.com" target="_blank">LunarNewYear.com</a></li>
-                <li><a href="https://pagodaasiangrill.com/" target="_blank">PagodaAsianGrill.com</a></li>
-                <li><a href="https://www.pfchangs.com/" target="_blank">PFChangs.com</a></li>
-                <li><a href="https://eptexas.com/" target="_blank">EPTexas.com</a></li>
-                <li><a href="https://bitcoin-current-price.com/" target="_blank">Bitcoin-Current-Price.com</a></li>
-                <li><a href="https://www.current-price.com/" target="_blank">Current-Price.com</a></li>
-                <li><a href="https://luckycatfund.com/" target="_blank">LuckyCatFund.com</a></li>
-                <li><a href="https://jasonleewilson.github.io/" target="_blank">jasonleewilson.github.io</a></li>
+                <li><a href="http://lunarnewyear.com" target="_blank" rel="noopener noreferrer">LunarNewYear.com</a></li>
+                <li><a href="https://pagodaasiangrill.com/" target="_blank" rel="noopener noreferrer">PagodaAsianGrill.com</a></li>
+                <li><a href="https://www.pfchangs.com/" target="_blank" rel="noopener noreferrer">PFChangs.com</a></li>
+                <li><a href="https://eptexas.com/" target="_blank" rel="noopener noreferrer">EPTexas.com</a></li>
+                <li><a href="https://bitcoin-current-price.com/" target="_blank" rel="noopener noreferrer">Bitcoin-Current-Price.com</a></li>
+                <li><a href="https://www.current-price.com/" target="_blank" rel="noopener noreferrer">Current-Price.com</a></li>
+                <li><a href="https://luckycatfund.com/" target="_blank" rel="noopener noreferrer">LuckyCatFund.com</a></li>
+                <li><a href="https://jasonleewilson.github.io/" target="_blank" rel="noopener noreferrer">jasonleewilson.github.io</a></li>
 
               </ul>
             </div>
