7dcf7582a2a78429e68ef4ff074fce0972a282d3
Jason Wilson <wilson@Jasons-Air.home>
2024-02-12 14:30:01 -0600

    form update

--- pages/contact.js
+++ pages/contact.js
@@ -33,7 +33,8 @@ export default function Contact() {
                 <br />
               </p>
               <div>
-              <form name="contact" method="post" netlify>
+              <form name="contact" method="post">
+              <input type="hidden" name="form-name" value="contact" />
                 <input className="d-block form-control" type="text" id="name" name="name" aria-label="name" placeholder="Name" required />
                 <input className="d-block form-control" type="email" id="email" name="email" aria-label="email" placeholder="Email" required />
                 <textarea className="d-block form-control" id="message" name="message" aria-label="message" placeholder="Message" required />
