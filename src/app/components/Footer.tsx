c157c602421b8747cbfffac1f74e3342d274dc38
Jason Wilson <wilson@Jasons-Air.home>
2024-04-04 13:32:01 -0500

    added mobile footer link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -16,36 +16,74 @@ export default function Footer() {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <footer className='grid sm:grid-cols-1 md:grid-cols-2 item-center justify-center p-4'>
-      <div className='flex flex-row justify-evenly sm:justify-center md:justify-start icons mb-4 md:mb-0'>
-        <div className='has-tooltip'>
-          <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
-            Linkedin
-          </span>
-          <a
-            href='https://www.linkedin.com/in/jasonleewilson/'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='linkedin'
-          >
-            <SiLinkedin className='linkedin' title='linkedin' />
-          </a>
-        </div>
-        <div className='has-tooltip'>
-          <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
-            X
-          </span>
-          <a
-            href='https://twitter.com/jasonleewilson'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='twitter'
-          >
-            <SiX className='twitter' title='twitter' />
-          </a>
-        </div>
+    <footer>
+      <div className='sm:block lg:hidden w-full text-center'>
+        <ul className='mt-4'>
+          <li className='py-4 w-full hover:bg-gray-200'>
+            <Link href='/'>
+              <p>Home</p>
+            </Link>
+          </li>
+          <li className='py-4 w-full hover:bg-gray-200'>
+            <Link href='/about'>
+              <p>About</p>
+            </Link>
+          </li>
+          <li className='py-4 w-full hover:bg-gray-200'>
+            <Link href='/work'>
+              <p>Work</p>
+            </Link>
+          </li>
 
-        {/* <a
+          <li className='py-4 w-full hover:bg-gray-200'>
+            <Link href='/skills'>
+              <p>Skills</p>
+            </Link>
+          </li>
+
+          <li className='py-4 w-full hover:bg-gray-200'>
+            <Link href='/faq'>
+              <p>FAQ</p>
+            </Link>
+          </li>
+
+          <li className='py-4 w-full hover:bg-gray-200'>
+            <Link href='/contact'>
+              <p>Contact</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 item-center justify-center p-4'>
+        <div className='flex flex-row justify-evenly sm:justify-center md:justify-start icons mb-4 md:mb-0'>
+          <div className='has-tooltip'>
+            <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
+              Linkedin
+            </span>
+            <a
+              href='https://www.linkedin.com/in/jasonleewilson/'
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label='linkedin'
+            >
+              <SiLinkedin className='linkedin' title='linkedin' />
+            </a>
+          </div>
+          <div className='has-tooltip'>
+            <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
+              X
+            </span>
+            <a
+              href='https://twitter.com/jasonleewilson'
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label='twitter'
+            >
+              <SiX className='twitter' title='twitter' />
+            </a>
+          </div>
+
+          {/* <a
           href='https://instagram.com/jasonleewilson_'
           target='_blank'
           rel='noopener noreferrer'
@@ -53,49 +91,50 @@ export default function Footer() {
         >
           <SiInstagram className='instagram' />
         </a> */}
-        <div className='has-tooltip'>
-          <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
-            Pinterest
-          </span>
-          <a
-            href='https://www.pinterest.com/jasonleewilson/'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='pinterest'
-          >
-            <SiPinterest className='pinterest' title='pinterest' />
-          </a>
-        </div>
+          <div className='has-tooltip'>
+            <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
+              Pinterest
+            </span>
+            <a
+              href='https://www.pinterest.com/jasonleewilson/'
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label='pinterest'
+            >
+              <SiPinterest className='pinterest' title='pinterest' />
+            </a>
+          </div>
 
-        <div className='has-tooltip'>
-          <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
-            Codepen
-          </span>
-          <a
-            href='https://codepen.io/jasonleewilson'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='codepen'
-          >
-            <SiCodepen className='codepen' title='codepen' />
-          </a>
+          <div className='has-tooltip'>
+            <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
+              Codepen
+            </span>
+            <a
+              href='https://codepen.io/jasonleewilson'
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label='codepen'
+            >
+              <SiCodepen className='codepen' title='codepen' />
+            </a>
+          </div>
+          <div className='has-tooltip'>
+            <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
+              Github
+            </span>
+            <a
+              href='https://github.com/jasonleewilson'
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label='github'
+            >
+              <SiGithub className='github' title='github' />
+            </a>
+          </div>
         </div>
-        <div className='has-tooltip'>
-          <span className='tooltip rounded shadow-lg p-1 bg-gray-100 text-primary -mt-8'>
-            Github
-          </span>
-          <a
-            href='https://github.com/jasonleewilson'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='github'
-          >
-            <SiGithub className='github' title='github' />
-          </a>
+        <div className='md:text-right text-gray-700'>
+          &#169; Copyright 1999-{year} jasonleewilson.com
         </div>
-      </div>
-      <div className='md:text-right text-gray-700'>
-        &#169; Copyright 1999-{year} jasonleewilson.com
       </div>
     </footer>
   );
