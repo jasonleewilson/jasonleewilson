0c721f0f498053ed36327fdcf86a814599ea456d
Jason Wilson <info@jasonleewilson.com>
2024-04-27 11:17:12 -0500

    update mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ import { RiArrowRightUpLine } from "react-icons/ri";
 
 export default function Home() {
   return (
-    <main className='flex-grow container-fluid mt-20 :px-4 lg:px-4'>
+    <main className='flex-grow container-fluid mt-20 lg:px-4'>
       <section className=' grid h-full place-items-center'>
-        <div className='mx-auto max-w-screen-xl px-4'>
+        <div className='mx-auto max-w-screen-xl px-2'>
           <div className='mx-auto max-w-xl text-center'>
-            <h1 className='text-6xl md:text-4xl font-extrabold'>
+            <h1 className='text-4xl md:text-4xl font-extrabold'>
               jasonleewilson.
               <strong className='font-extrabold text-primary sm:block '>
                 {" "}
@@ -25,14 +25,14 @@ export default function Home() {
               </strong>
             </h1>
 
-            <h2 className='mt-4 leading-10 text-3xl/relaxed md:text-2xl'>
+            <h2 className='mt-4 leading-6 text-xl/relaxed md:text-2xl'>
               I build pixel-perfect, engaging, and accessible digital
               experiences.
             </h2>
 
             <div className='mt-8 flex flex-wrap justify-center gap-4'>
               <Link
-                className='block w-full rounded bg-primary hover:bg-transparent hover:text-primary hover:outline hover:outline-2 hover:outline-primary px-12 py-3 text-2xl md:text-sm uppercase font-normal text-white focus:outline-none focus:ring active:bg-primary sm:w-auto'
+                className='block w-full rounded bg-primary hover:bg-transparent hover:text-primary hover:outline hover:outline-2 hover:outline-primary px-12 py-3 text-xl md:text-sm uppercase font-normal text-white focus:outline-none focus:ring active:bg-primary sm:w-auto'
                 href='/work'
               >
                 View my work{" "}
@@ -44,7 +44,7 @@ export default function Home() {
               </Link>
 
               <Link
-                className='block w-full rounded px-12 py-3 text-2xl md:text-sm uppercase font-normal text-primary outline outline-2 outline-primary hover:text-primary focus:outline-2 focus:ring active:text-primary sm:w-auto'
+                className='block w-full rounded px-12 py-3 text-xl md:text-sm uppercase font-normal text-primary outline outline-2 outline-primary hover:text-primary focus:outline-2 focus:ring active:text-primary sm:w-auto'
                 href='/about'
               >
                 About me{" "}
