f0b56bda387fee624a4b0823de2e02c7a906b442
Jason Wilson <robotici@yahoo.com>
2026-04-21 21:51:51 -0500

    update port

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -104,11 +104,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.4,
     },
-    {
-      url: `${baseUrl}/work/details-pressonthese`,
-      lastModified: now,
-      changeFrequency: 'monthly',
-      priority: 0.4,
-    },
+    // {
+    //   url: `${baseUrl}/work/details-pressonthese`,
+    //   lastModified: now,
+    //   changeFrequency: 'monthly',
+    //   priority: 0.4,
+    // },
   ];
 }
